502af37bc8936484b4766cad919496cb701069e4
Jose Ignacio Camacho Velasquez <jcamacho@dcatech.com>
2019-02-18 08:24:14 -0500

    Update of varibales var -> const

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
-var path = require('path');
-var express = require('express');
+const path = require('path');
+const express = require('express');
 
-var app = express();
+const app = express();
 
-var staticPath = path.join(__dirname, '/'); // Main file path.
+const staticPath = path.join(__dirname, '/'); // Main file path.
 app.use(express.static(staticPath));
 
 // Allows you to set port in the project properties.
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
     console.log(`listening in port ${port}`);
 });
